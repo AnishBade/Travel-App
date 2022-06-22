c3465a5349545cafb9b196c93a89cc3a0cecb730
AnishBade <badeanish6045@gmail.com>
2022-06-22 12:08:38 +0545

    made dynamic, just before using database

--- travello_app/models.py
+++ travello_app/models.py
@@ -1,3 +1,11 @@
+from turtle import st
 from django.db import models
 
 # Create your models here.
+class Destination:
+    id : int
+    name : str 
+    img : str 
+    desc : str
+    price : int
+    offer : bool 
