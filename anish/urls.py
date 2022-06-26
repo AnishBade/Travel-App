ada172bfad1ace911333654a70f968c1e89b24c5
AnishBade <badeanish6045@gmail.com>
2022-06-26 09:11:12 +0545

    new app registration started

--- anish/urls.py
+++ anish/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     
     path('calc/',include('calculator_app.urls')),
     path('admin/', admin.site.urls),
-    path('',include('travello.urls'))
+    path('',include('travello.urls')),
+    path('accounts/',include('accounts.urls'))
 ]
 urlpatterns= urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
